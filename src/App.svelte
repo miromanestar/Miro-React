c420836645578593903c38c0070a6c1a768d1ede
Miro Manestar <miroimanestar@gmail.com>
2021-11-10 22:11:07 -0500

    Continued playing with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,19 @@
 <script>
-    import {
-        ThemeProvider as MUIThemeProvider,
-        StyledEngineProvider,
-        createTheme
-    } from '@mui/material/styles'
-    import { CssBaseline } from '@mui/material'
     import { Router, Link, Route } from 'svelte-routing'
     import Home from './pages/Home.svelte'
-
+    import Header from './components/Header.svelte'
     export const url = '';
-
-    const theme = createTheme({
-        palette: {
-            mode: 'dark'
-        }
-    })
-
 </script>
+
+
 <Router url={url}>
-        <MUIThemeProvider theme={theme}>
-        </MUIThemeProvider>
+        <Header />
+
         <main>
             <Route path="/"><Home /></Route>
         </main>
 </Router>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
+
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from '@smui/icon-button'
+  import Button, { Label } from '@smui/button';
+
+  import Drawer, {
+    AppContent,
+    Content,
+    Header,
+    Subtitle,
+    Scrim,
+  } from '@smui/drawer';
+  import List, { Item, Text, Graphic, Separator, Subheader } from '@smui/list';
+  import { H6 } from '@smui/common/elements';
+
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
+</script>
+
+<main>
+<TopAppBar
+  variant="static"
+  {prominent}
+  {dense}
+>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Flex Static</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+</main>
+
+<style>
+
+</style>
